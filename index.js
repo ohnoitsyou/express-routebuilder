9d0c4f9a69832d251edb1ae96f00ee1f65420410
David Young <ohnoitsyou7043@gmail.com>
2016-01-21 18:53:45 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/* Take in an array of routes structured thusly:
+ *   {
+ *     '/': {
+ *       'get': function (req, res) {
+ *       },
+ *       'post': function (req, res) {
+ *       }
+ *     }
+ *     '/users' : {
+ *       'get': function (req, res) {
+ *       },
+ *       'delete': function (req, res) {
+ *       }
+ *     }
+ *   }
+ *
+ * and construct an express router
+ * Allows the methods routes to be easily unit-tested later
+ * I've chosen to implement the basic 4, get, post, put, delete, for now.
+ *   Feel free to expand if needed
+ */
+var express = require('express')
+var router = express.Router()
+
+function routebuilder (routes) {
+  var endpoints = Object.keys(routes)
+  for (var endpoint of endpoints) {
+    var methods = Object.keys(routes[endpoint])
+    for (var method of methods) {
+      var cb = routes[endpoint][method]
+      switch (method) {
+        case 'all':
+          router.all(endpoint, cb)
+          break
+        case 'get':
+          router.get(endpoint, cb)
+          break
+        case 'post':
+          router.post(endpoint, cb)
+          break
+        case 'put':
+          router.put(endpoint, cb)
+          break
+        case 'delete':
+          router.delete(endpoint, cb)
+          break
+        default:
+          break
+      }
+    }
+  }
+  return router
+}
+
+exports = module.exports = routebuilder
